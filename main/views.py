821e36d5e3cd501cf5e67302caf7d1b0f4724e51
Shubham <shubham.singh@renewbuy.com>
2020-09-25 14:53:05 +0530

    Proper Output

--- main/views.py
+++ main/views.py
@@ -11,6 +11,14 @@ def get_data(request):
     members = Member.objects.all()
     if members:
         serialized_data = MemberSerializer(members, many=True)
-        return Response(serialized_data.data, status=status.HTTP_200_OK)
-    return Response("No Data Available", status=status.HTTP_204_NO_CONTENT)
+        data = {
+            'ok': True,
+            "members" : serialized_data.data
+        }
+        return Response(data=data, status=status.HTTP_200_OK)
+    data = {
+            'ok': False,
+            "Detail" : "No Data Available"
+    }
+    return Response(data=data, status=status.HTTP_204_NO_CONTENT)
 
